e9ab7de2b0b09e8172bc78f520d5669dd50d14c9
“pinki <pinkijha1310@email.com>
2024-05-20 19:53:21 +0530

    siblings of inside neseted structure(child 1& 2) in react with [] brackets

--- app.js
+++ app.js
@@ -21,11 +21,16 @@ root.appendChild(heading); */
 
 // how to create nested structure in react
     const parent = React.createElement("div", {id:'parent'}, 
-                   React.createElement("div", {id:'child'},
-                   [React.createElement("h1", {}, 'I am an h1 tag'),
+    [
+                   React.createElement("div", {id:'child1'},
+                   [React.createElement("h1", {}, 'I am an h1 tag from child 1'),
                    React.createElement("p", {}, 'I am an Paragraph tag')]
-                    )                   
-    );
+                    ) ,
+                    React.createElement("div", {id:'child2'},
+                   [React.createElement("h1", {}, 'I am an h1 tag from child 2'),
+                   React.createElement("p", {}, 'I am an Paragraph tag')]
+                    )                  
+                ]);
 
 // React Code
 const heading = React.createElement("h1", {id:'heading', name:'pinki'}, "Hello from React");
