28538ae7dfbfb92386af746bcb4bb9e27e08ea5f
“pinki <pinkijha1310@email.com>
2024-05-20 19:14:43 +0530

    nested kind of structure in react

--- app.js
+++ app.js
@@ -6,8 +6,30 @@ heading.innerHTML = "hello world";
 const root = document.querySelector(".root");
 root.appendChild(heading); */
 
+
+/* 
+*   how to create nested element in html
+*    <div class="parent">
+    <div class="child">
+        <h1></h1>
+    </div>
+    </div>
+*
+*
+*
+*/
+
+// how to create nested structure in react
+    const parent = React.createElement("div", {id:'parent'}, 
+                   React.createElement("div", {id:'child'},
+                   React.createElement("div", {}, 'I am an h1 tag')
+                    )
+                   
+    );
+
 // React Code
 const heading = React.createElement("h1", {id:'heading', name:'pinki'}, "Hello from React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 console.log(heading);
-root.render(heading);
+console.log(parent);
+root.render(parent);
