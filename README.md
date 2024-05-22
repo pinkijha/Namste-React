ffc32c28a078c69470e780d9d0937699e6b72c62
“pinki <pinkijha1310@email.com>
2024-05-23 01:45:11 +0530

    add parcel build parcel edit package.json file

---
+++ README.md
@@ -0,0 +1,22 @@
+# React
+
+
+# Parcel
+- Dec build
+- Local server
+- HMR = Hot Module Replacement
+- File Watching Algorithm -- Written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification 
+- Bundling
+- Compress
+- Consitent Hashing
+- Code Splitting
+- Differential Bundling - Support older Browsers
+- Diagnostic - Beautiful Error Lookwise
+- Error Handling
+- HTTPS
+- Tree Shaking - Remove unused code for you
+- Different dev and prod bundles
+- 
--- app.js
+++ app.js
@@ -1,39 +1,18 @@
-
-// JavaScript Code
-/* const heading = document.createElement("h1");
-heading.innerHTML = "hello world";
-
-const root = document.querySelector(".root");
-root.appendChild(heading); */
-
-/**
- * how to create nested element in html
-*    <div class="parent">
-    <div class="child">
-        <h1></h1>
-    </div>
-    </div>
- * 
- * 
- * 
-
-
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 // how to create nested structure in react
     const parent = React.createElement("div", {id:'parent'}, 
     [
                    React.createElement("div", {id:'child1'},
-                   [React.createElement("h1", {}, 'I am an h1 tag from child 1'),
-                   React.createElement("p", {}, 'I am an Paragraph tag')]
+                   [React.createElement("h1", {}, 'Pinki Jha '),
+                   React.createElement("p", {}, 'Right now i am learning React')]
                     ) ,
                     React.createElement("div", {id:'child2'},
                    [React.createElement("h1", {}, 'I am an h1 tag from child 2'),
                    React.createElement("p", {}, 'I am an Paragraph tag')]
                     )                  
-                ]); */
-
-
+                ]); 
 
 // React Code
 const heading = React.createElement("h1", {id:'heading', name:'pinki'}, "Hello from React");
@@ -42,5 +21,5 @@ console.log(heading);
 console.log(parent);
 root.render(parent);
 
-const img = React.createElement('img', {src: 'https://www.india.com/wp-content/uploads/2021/03/bengaluru-feature-image-Twitter-1200.jpg', alt:'flowers'}, )
-root.render(img);
+// const img = React.createElement('img', {src: 'https://www.india.com/wp-content/uploads/2021/03/bengaluru-feature-image-Twitter-1200.jpg', alt:'flowers'}, )
+// root.render(img);
