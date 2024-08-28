db33c80bf56f95ad99e186742904e557ef95cd60
“pinki <pinkijha1310@email.com>
2024-08-28 21:32:37 +0530

    add Sum.js component and use test cases to test the component

---
+++ projects/khaokhilao/src/Components/__test__/sum.test.js
@@ -0,0 +1,9 @@
+import { Sum } from "../Sum";
+
+
+test("Sum function should calculate the sum of two numbers" , () => {
+    const result = Sum(3,5);
+
+    //Assertion
+    expect(result).toBe(5);
+});
